7fd755821b6452eed65a0de2fbdff3e1aaec101d
ayana1709 <ayanabashachali@gmail.com>
2024-04-23 23:08:43 +0300

    building the sidebar and main navigation

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
 
-            <Route path="booking" element={<Bookings />} />
+            <Route path="bookings" element={<Bookings />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,12 +1,19 @@
 import styled from "styled-components";
-
+import { NavLink } from "react-router-dom";
+import {
+  HiOutlineCalendarDays,
+  HiOutlineCog6Tooth,
+  HiOutlineHome,
+  HiOutlineHomeModern,
+  HiOutlineUsers,
+} from "react-icons/hi2";
 const NavList = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 0.8rem;
 `;
 
-const Link = styled.a`
+const StyledNavLink = styled(NavLink)`
   &:link,
   &:visited {
     display: flex;
@@ -44,3 +51,43 @@ const Link = styled.a`
     color: var(--color-brand-600);
   }
 `;
+function MainNav() {
+  return (
+    <nav>
+      <NavList>
+        <li>
+          <StyledNavLink to="./dashboard">
+            <HiOutlineHome />
+            <span>Home</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="./bookings">
+            <HiOutlineCalendarDays />
+            Bookings
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="./cabins">
+            <HiOutlineHomeModern />
+            <span>Cabins</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="./users">
+            <HiOutlineUsers />
+            <span>User</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="./settings">
+            <HiOutlineCog6Tooth />
+            <span>Setting</span>
+          </StyledNavLink>
+        </li>
+      </NavList>
+    </nav>
+  );
+}
+
+export default MainNav;
