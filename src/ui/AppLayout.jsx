334d42dff8359beaf2e542dff9191fb5a4a1b2d6
ayana1709 <ayanabashachali@gmail.com>
2024-04-23 17:18:36 +0300

    building the App Layout

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,29 @@
+import { Outlet } from "react-router-dom";
+import SideBar from "./SideBar";
+import Header from "./Header";
+import styled from "styled-components";
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+function AppLayout() {
+  return (
+    <StyledAppLayout>
+      <Header />
+
+      <SideBar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
+  );
+}
+
+export default AppLayout;
