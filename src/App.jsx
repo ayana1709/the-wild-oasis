ba66e838ea419d4f8fd228a4351fd1c8927e0e36
ayana1709 <ayanabashachali@gmail.com>
2024-04-23 10:34:21 +0300

    Building more reusable styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,43 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
-import { Button } from "./ui/Button";
+import Button from "./ui/Button";
 import Input from "./ui/Input";
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
 function App() {
   return (
-    <StyledApp>
+    <>
       <GlobalStyles />
-      <H1>the wild oasis</H1>
-      <Button onClick={() => alert("check in")}>check in</Button>
-      <Button onClick={() => alert("check out")}>check out</Button>
-      <Input type="number" placeholder="Number of guests" />
-      <Input type="number" placeholder="Number of guests" />
-    </StyledApp>
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">the wild oasis</Heading>
+            <div>
+              <Heading as="h2">check in and out</Heading>
+              <Button onClick={() => alert("check in")}>check in</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("check out")}
+              >
+                check out
+              </Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <Heading as="h3">Form </Heading>
+            <Input type="number" placeholder="Number of guests" />
+            <Input type="number" placeholder="Number of guests" />
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
   );
 }
 
